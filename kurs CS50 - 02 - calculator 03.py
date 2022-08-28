99934d6867c114dccd761c98dad758fca7756508
Mrk-Mzj <Marek.Mazij@gmail.com>
2022-08-28 18:09:21 +0200

    function name changed

--- kurs CS50 - 02 - calculator 03.py
+++ kurs CS50 - 02 - calculator 03.py
@@ -6,14 +6,14 @@ def main():
     y = float(input("What's Y? "))
 
     # in case of adding
-    result = cleaning(x+y)
+    result = cleaned(x+y)
 
     # printing the result
     print(f'\n Result is: {result} \n')
 
 
 
-def cleaning(dirt):
+def cleaned(dirt):
       
     #convering to string (f''), separating tousands with commas(:,), replacing commas with spaces, cleaning '.0'
     return (f'{dirt:,}').replace(',' , ' ').replace('.0' , '')
