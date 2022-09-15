0f5f34a25a206f5f6f198e6dca95cdf66227e423
Mrk-Mzj <Marek.Mazij@gmail.com>
2022-09-15 16:41:19 +0200

    batch rename

--- CS50/06 - modules 02 - PACKAGES.py
+++ CS50/06 - modules 02 - PACKAGES.py
@@ -1,11 +1,14 @@
 import cowsay
 import sys
+
 print(dir(cowsay))
 # instalujemy pakiet Cowsay (zewnętrzny folder z modułami) poleceniem: "pip install cowsay". Więcej ciekawych pakietów na PyPI.org
-# uruchamiamy z konsoli: python "kurs CS50 - 06 - modules 02 - PACKAGES.py"
+"""uruchamiamy z konsoli: 
+python "06 - modules 02 - PACKAGES.py"
+"""
 
 # jeśli program zostaje uruchomiony z dopisanym dokładnie 1 parametrem
-if len (sys.argv) == 2:
+if len(sys.argv) == 2:
 
     # uruchom funkcję cow z pakietu cowsay
     cowsay.cow("Hello " + sys.argv[1])
