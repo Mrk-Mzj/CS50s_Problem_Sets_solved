1dfd6495c21681f99ac06c497521d7a8b508320d
Mrk-Mzj <Marek.Mazij@gmail.com>
2022-09-26 22:25:44 +0200

    kom mit

--- CS50/10 - object oriented programming/cs_10_oop_04 - class methods.py
+++ CS50/10 - object oriented programming/cs_10_oop_04 - class methods.py
@@ -6,43 +6,41 @@ class Student:
     # __init__ to funkcja, którą zawsze wywołuje konstruktor obiektu.
     # Przyjmuje obiekt, który właśnie powstaje, i parametry.
 
-    # Parametry opisuję dla czytelności jako name, name_init i name_inputed,
+    # Parametry opisuję dla czytelności jako name i name_inputed,
     # ale tak naprawdę wszędzie może być to samo "name".
 
-    def __init__(self, name_init, house_init):
+    def __init__(self, name_inputed, house_inputed):
 
         # Przypisujemy atrybuty (zmienne instancji) do obiektu.
         # Zasilamy pusty dotąd obiekt parametrami
-        self.name = name_init
-        self.house = house_init
+        self.name = name_inputed
+        self.house = house_inputed
 
 
 def main():
+
+    print()
     student = get_student()
     print(f"\n{student.name} is from {student.house}\n")
 
 
 def get_student():
-    print()
 
     # Poprzednio:
-
     # student = Student()
     # student.name = input("Name: ")
     # student.house = input("House: ")
 
     name_inputed = input("Name: ")
     house_inputed = input("House: ")
-    student = Student(name_inputed, house_inputed)
+    return Student(name_inputed, house_inputed)
 
     # Po nowemu jest czytelniej, bo przypisania przenieśliśmy do klasy.
     # Dodatkowo ten zapis pozwala skorzystać z metod klasy,
     # np. sprawdzających poprawność wprowadzonych danych.
 
     # BTW: Student(name, house) nazywamy wywołeniem konstruktora (constructor call)
-    # Ten kon skonstruuje dla nas obiekt.
-
-    return student
+    # Ten konstruuje dla nas obiekt.
 
 
 if __name__ == "__main__":
