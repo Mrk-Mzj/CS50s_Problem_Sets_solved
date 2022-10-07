ec6e4506b68df4343aa65e13d075ee7da1ff0092
Mrk-Mzj <Marek.Mazij@gmail.com>
2022-10-07 18:11:36 +0200

    working version!

--- CS50/uniwersytety - szukanie.py
+++ CS50/uniwersytety - szukanie.py
@@ -15,7 +15,6 @@ def main():
         "Stanford",
         "Yale",
     ]
-
     print("\n", uniwersytety)
     szukany = input("\nKtórego szukamy?\n")
     print()
@@ -32,41 +31,43 @@ def main():
             break
         miejsce += 1
 
-    # 2 - szukanie przy pomocy dzielenia na połowy (algorytm binarny):
-    # Zmierz długość listy. Podziel na pół. Zaokrąglij do int.
-    # !Round zaokrągla do liczb parzystych, unikając kumulowania się błędów zaokrągleń.
+    # 2 - szukanie przy pomocy dzielenia na połowy (algorytm binarny).
+    # Szukamy wyrazu najbliżej środka stawki. Sprawdzamy go.
 
-    miejsce = 0
-    while len(uniwersytety) > 1:
-        miejsce = srodek_listy(uniwersytety, miejsce)
+    srodkowy = 0  # inicjuję dowolną cyfrą, na potrzeby rozruchu pętli
 
-        # Porównaj środkowy element listy z szukanym:
-        # - jeśli środkowy == szukany, wypisz jego lokalizację
-        if uniwersytety[miejsce] == szukany:
+    while len(uniwersytety) > 0:
+        srodkowy = srodek_listy(uniwersytety, srodkowy)
+
+        # - jeśli szukany == środkowy, ogłoś sukces
+        if szukany == uniwersytety[srodkowy]:
             print(f"#2 Znalazłem {szukany}!")
             break
 
-        # - jeśli środkowy > szukany, skróć listę do pierwszej połowy
-        elif uniwersytety[miejsce] > szukany:
+        # - jeśli szukany < środkowy, skróć listę do pierwszej połowy
+        elif szukany < uniwersytety[srodkowy]:
             print(f"#2 {szukany} jest w pierwszej części listy:")
-            uniwersytety = uniwersytety[0:miejsce]
+            uniwersytety = uniwersytety[0:srodkowy]
             print(uniwersytety)
 
-        # - jeśli środkowy < szukany, skróć listę do drugiej połowy
+        # - jeśli szukany > środkowy, skróć listę do drugiej połowy
         else:
             print(f"#2 {szukany} jest w drugiej części listy:")
-            uniwersytety = uniwersytety[miejsce + 1 : len(uniwersytety)]
+            uniwersytety = uniwersytety[srodkowy + 1 : len(uniwersytety)]
             print(uniwersytety)
 
-    # + mierzenie czasu
     print()
 
 
-def srodek_listy(uniwersytety, miejsce):
-    miejsce = round(len(uniwersytety) / 2)
-    print(f"\n#2 Środek jest w {uniwersytety[miejsce]}")
-    return miejsce
+def srodek_listy(uniwersytety, srodkowy):
+    # Zmierz długość listy. Podziel na pół. Zaokrąglij do int.
+    # Round zaokrągla do liczb parzystych, unikając kumulowania się błędów zaokrągleń.
+    srodkowy = round(len(uniwersytety) / 2)
+    print(f"\n#2 Środek jest w {uniwersytety[srodkowy]}")
+    return srodkowy
 
 
 if __name__ == "__main__":
     main()
+
+# możesz teraz usunąć nadmiarowe printy i porównać czas działania algorytmów
