3ac11bb3580be2da0ec53ae334077d91dd5bbaf8
Mrk-Mzj <Marek.Mazij@gmail.com>
2022-10-17 12:19:00 +0200

    comm

--- CS50/11 - Et Cetera/cs_11_etc_04_unpacking.py
+++ CS50/11 - Et Cetera/cs_11_etc_04_unpacking.py
@@ -1,5 +1,8 @@
 # Rozpakowywanie wartości do kilku zmiennych
 
+# 1, z użyciem listy:
+# Zapis zmiennej *coins z gwiazdką rozpakowuje ją do poszczególnych wartości:
+
 
 def total(dollars, euros, plns):
     return (dollars * 17 + euros) * 29 + plns
@@ -7,5 +10,19 @@ def total(dollars, euros, plns):
 
 coins = [100, 50, 25]
 
-# zapis zmiennej z gwiazdką rozpakowuje ją do poszczególnych wartości:
 print(total(*coins))
+
+
+# 2, z użyciem słownika. Dwie gwiazdki sięgają po klucz i po wartość:
+
+coins = {"dollars": 100, "euros": 50, "plns": 25}
+print(total(**coins))
+
+# Pojedyncza gwiazdka służy do sięgania po sam klucz:
+
+
+def waluty(waluta1, waluta2, waluta3):
+    print(f"waluty to {waluta1}, {waluta2}, {waluta3}\n")
+
+
+waluty(*coins)
---
+++ CS50/11 - Et Cetera/cs_11_etc_05_args_kwargs.py
@@ -0,0 +1,27 @@
+# Przekazywanie zmiennej liczby argumentów do funkcji
+
+# args to argumenty pozycyjne, zależne od miejsca. Jak w listach
+# rozpakowywanych do funkcji (patrz: poprzedni plik).
+
+# kwargs (keyword args) to argumenty słów kluczowych. Jak w słownikach.
+
+
+# Napiszmy funkcję która przyjmuje zmienną liczbę argumentów pozycyjnych
+# i argumentów słów kluczowych:
+
+
+def f(*args, **kwargs):
+    print("Positional:", args)
+    print("Named:", kwargs)
+    print()
+
+
+f(100, 50, 25)  # positional args; tuple
+f(dollars=100, euros=50, plns=25)  # named kwargs; dict
+f(100, 50, plns=25)  # both
+
+# Funkcja może rozpakować otrzymane tuple z args i słowniki z kwargs,
+# dzięki gwiazdce:
+
+#    print("Positional:", *args)
+#    print("Named:", *kwargs)
