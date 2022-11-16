0e0b45c1423e62b5b7461bc2c6695a7c061e0711
Mrk-Mzj <Marek.Mazij@gmail.com>
2022-11-16 22:01:51 +0100

    code got commented

--- CS50X/09__Flask/09_10__store/app.py
+++ CS50X/09__Flask/09_10__store/app.py
@@ -1,11 +1,10 @@
+# Dodawanie książek do koszyka zakupowego.
+
 from cs50 import SQL
 from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
 
-# Configure app
 app = Flask(__name__)
-
-# Connect to database
 db = SQL("sqlite:///store.db")
 
 # Configure session
@@ -14,6 +13,11 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 
+# Root wyświetla wszystkie książki z bazy danych.
+# Dowolną z nich można dodać do koszyka przyciskiem.
+# Przycisk działa identycznie, jak formularz
+# z niewidocznym polem w którym z góry wpisany jest ID książki z bazy.
+# Kliknięcie przycisku kieruje metodą POST do /cart.
 @app.route("/")
 def index():
     books = db.execute("SELECT * FROM books")
@@ -23,17 +27,20 @@ def index():
 @app.route("/cart", methods=["GET", "POST"])
 def cart():
 
-    # Ensure cart exists
+    # Stwórz pusty koszyk, jeżeli nie zrobiliśmy tego wcześniej
+    # przy dodawaniu innej książki:
     if "cart" not in session:
         session["cart"] = []
 
-    # POST
+    # POST - czyli ktoś wszedł z formularza. Powinniśmy znać ID dodanej książki.
+    # Jeśli rzeczywiście tak jest, dodajmy ją do ciastka sesji i przeładujmy stronę:
     if request.method == "POST":
         id = request.form.get("id")
         if id:
             session["cart"].append(id)
         return redirect("/cart")
 
-    # GET
+    # GET - ktoś wszedł bezpośrednio, albo z przeładowania po dodaniu książki.
+    # Sprawdźmy nazwy książek w koszyku na podstawie ich ID z ciastka i wylistujmy:
     books = db.execute("SELECT * FROM books WHERE id IN (?)", session["cart"])
     return render_template("cart.html", books=books)
