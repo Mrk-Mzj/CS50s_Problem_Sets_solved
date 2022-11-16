a99d73cb71f922bc42681b2d9dbf68632d5354aa
Mrk-Mzj <Marek.Mazij@gmail.com>
2022-11-17 00:53:16 +0100

    1st comm

---
+++ CS50X/09__Flask/09_11_search simple/app.py
@@ -0,0 +1,22 @@
+# Proste wyszukiwanie filmów, których tytuły zawierają szukane słowo.
+# Metoda GET. Bez autouzupełniania. Nic nowego.
+
+from cs50 import SQL
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+db = SQL("sqlite:///shows.db")
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/search")
+def search():
+    shows = db.execute(
+        "SELECT * FROM shows WHERE title LIKE ?", "%" + str(request.args.get("q")) + "%"
+    )
+    return render_template("search.html", shows=shows)
