7c676a89c8166e4862f20851d58d0ef292e67f6f
Mrk-Mzj <Marek.Mazij@gmail.com>
2022-11-17 23:41:06 +0100

    comments and folder names updates

--- CS50X/09__Flask/09_13_search_AJAX_JSON_autocomplete/app.py
+++ CS50X/09__Flask/09_13_search_AJAX_JSON_autocomplete/app.py
@@ -1,13 +1,17 @@
 # Szukanie z użyciem Ajax i JSON. Warto przeczytać najpierw plik 09_12.
 
 # Poprzedni kod działał poprawnie, ale pod maską przesyłał mało elegancką listę
-# znaczników <li></li> i tytułów między nimi, czytelną tylko dla HTMLa.
+# znaczników <li></li> i tytułów między nimi, jako XML czytelny tylko dla HTMLa.
+# Używanie XML w Ajax To stara szkoła.
 
 # Obecnie API piszę się tak, by przesyłały dane w uniwersalnym dla wielu języków formacie JSON,
 # czyli listę słowników: [{"id":123, "title":abc}, {"id":456, "title":def}].
+# JSON łatwiej przeformatować dla potrzeb Java Scriptu czy Pythona.
+
 
 from cs50 import SQL
 from flask import Flask, jsonify, render_template, request
+
 app = Flask(__name__)
 
 db = SQL("sqlite:///shows.db")
