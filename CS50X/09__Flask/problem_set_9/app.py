686484a0476ac72facc0d74bdefd3bb5045d7551
Mrk-Mzj <Marek.Mazij@gmail.com>
2022-11-18 22:45:38 +0100

    comments update

--- CS50X/09__Flask/problem_set_9/app.py
+++ CS50X/09__Flask/problem_set_9/app.py
@@ -1,3 +1,10 @@
+# API_KEY zapisałem w pliku tekstowym. Ustawiamy go w cmd:
+# set API_KEY=pk_3b6cf277dec74af49ff62d8c0f2e22d4
+# i restartujemy cmd.
+# Dla bash i powershell komendy brzmią trochę inaczej.
+# Zmienne można też wpisać na stałe, w ustawieniach systemu.
+
+
 import os
 
 from cs50 import SQL
@@ -6,7 +13,9 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
+# funkcje importowane z helpers.py:
 from helpers import apology, login_required, lookup, usd
+
 
 # Configure application
 app = Flask(__name__)
@@ -15,9 +24,12 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Custom filter
+# usd to funkcja w helpers.py; ułatwia formatowanie walut.
 app.jinja_env.filters["usd"] = usd
 
-# Configure session to use filesystem (instead of signed cookies)
+# Konfiguruj aplikację by ciastka sesji były trzymane w lokalnym systemie, np. na dysku.
+# Tak, jak robiliśmy to już wcześniej. Flask na domyślnych ustawieniach tworzyłby sesje
+# w cyfrowo podpisanych ciastkach.
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
@@ -33,12 +45,15 @@ if not os.environ.get("API_KEY"):
 @app.after_request
 def after_request(response):
     """Ensure responses aren't cached"""
+    # Wyłączamy cache by zmiany, jakie robimy w plikach, były zawsze widziane przez przeglądarkę
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
 
 
+# Login_required to funkcja dekorująca, stworzona w helpers.py.
+# Przekierowuje niezalogowanych do strony logowania.
 @app.route("/")
 @login_required
 def index():
@@ -60,6 +75,8 @@ def history():
     return apology("TODO")
 
 
+# Login zamienia wprowadzone hasło na hasz. Porównuje go z haszem w bazie danych.
+# Tworzy ciastko z id użytkownika. W ten sposób ścieżki wiedzą, który user jest zalogowany.
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
