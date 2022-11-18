8c80f86e772742fb71292a70b68974a5012ca7fd
Mrk-Mzj <Marek.Mazij@gmail.com>
2022-11-18 15:52:01 +0100

    Pylance problems ignored, API trial key obtained

--- CS50X/09__Flask/problem_set_9/app.py
+++ CS50X/09__Flask/problem_set_9/app.py
@@ -79,10 +79,13 @@ def login():
             return apology("must provide password", 403)
 
         # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):  # type: ignore
+            # powyższy dopisek to info do Pylance by nie podkreślał błędu w kodzie od autorów.
             return apology("invalid username and/or password", 403)
 
         # Remember which user has logged in
