66ece57532856d8fec7e373cc75c30e5770d066a
Mrk-Mzj <Marek.Mazij@gmail.com>
2022-12-04 18:46:49 +0100

    comments update

--- CS50P/03__Exceptions/cs__03_01__printing_EXCEPTIONS.py
+++ CS50P/03__Exceptions/cs__03_01__printing_EXCEPTIONS.py
@@ -1,21 +1,17 @@
-
-
-# odpytujmy usera w nieskończoność, dopóki nie poda prawidłowej odpowiedzi
+# Odpytujmy usera w nieskończoność, dopóki nie poda integera. Wtedy break wyskoczy z pętli.
+# W przypadku błędnej wartości piszemy ostrzeżenie i pętla rusza od nowa.
 
 while True:
     try:
         x = int(input("What's x? "))
-        print(f'X is {x}')
+        print(f"X is {x}")
         break
-    
-    # w przypadku błędnej wartości piszemy ostrzeżenie i pętla rusza od nowa
-    
+
+    except ValueError:
+        print("X is not an integer! \n")
+
     # wypisujemy tu konkretny typ błędu, jaki może zrobić user (ValueError)
     # brak dokładnej deklaracji jest dopuszczalny, ale to zła praktyka; może utrudnić debugowanie
-    
-    except ValueError:
-        print('X is not an integer! \n')
 
 
-print('End \n')
-
+print("End \n")
