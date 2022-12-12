6e5ff16bb2876cd08382bea03c4b421ca6101f6f
Mrk-Mzj <Marek.Mazij@gmail.com>
2022-12-13 00:13:59 +0100

    test finished, got run, program has passed it

--- CS50P/05__Unit_Tests/problem_set_5/twitter/test_twitter.py
+++ CS50P/05__Unit_Tests/problem_set_5/twitter/test_twitter.py
@@ -1,1 +1,31 @@
+# pip install -U pytest
+# pytest test_twitter.py
+
+
 from twitter import shorten
+
+
+def test_numbers_shorten():
+
+    assert shorten("123") == "123"
+    assert shorten("15.2") == "15.2"
+    assert shorten("0") == "0"
+    assert shorten("-15.2") == "-15.2"
+    assert shorten("-123") == "-123"
+
+
+def test_specials_shorten():
+    assert shorten("!@#%f\n") == "!@#%f\n"
+    assert shorten(" ") == " "
+
+
+def test_small_letters_shorten():
+    assert shorten("abba") == "bb"
+    assert shorten("ez gg") == "z gg"
+    assert shorten("aeiouy") == ""
+
+
+def test_large_letters_shorten():
+    assert shorten("ABBA") == "BB"
+    assert shorten("EZ GG") == "Z GG"
+    assert shorten("AEIOUY") == ""
