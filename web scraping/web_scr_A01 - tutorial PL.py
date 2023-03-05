7cac564dfbe76c01c0fc1196589f1ebe586ca1fd
Mrk-Mzj <Marek.Mazij@gmail.com>
2023-03-05 22:42:40 +0100

    tutorial completed

--- web scraping/web_scr_A01 - tutorial PL.py
+++ web scraping/web_scr_A01 - tutorial PL.py
@@ -2,13 +2,51 @@
 
 # pip install beautifulsoup4
 # pip install lxml
+# pip install prettyprint
 
 
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 
 request = requests.get("http://quotes.toscrape.com/")
-
 soup = BeautifulSoup(request.text, "lxml")
-print(soup.prettify())
+print()
+
+# drukowanie całego HTML:
+# print(soup.prettify())
+
+
+# wybór klasy author:
+names = soup.select(".author")
+
+
+# drukowanie klasy. Wynikiem jest pozioma lista na szerokość strony:
+print(names, "\n")
+
+
+# drukowanie z pretty print. Wynikiem jest pionowa lista podzielona enterami
+# (z nawiasem klamrowym i przecinkami):
+pprint.pprint(names)
+print()
+
+
+# drukowanie. Wynikiem są pionowo wypisane elementy listy
+# (bez nawiasu kwadratowego i przecinków).
+# Dodatkowo .text formatuje wynik do samych nazwisk (bez tagów HTML). Niestety są duplikaty:
+for _ in names:
+    print(_.text)
+
+
+# Drukowanie bez dupikatów i alfabetycznie.
+# Stwórzmy set authors_names i dodajmy do niego każdą linijkę.
+# Wydrukujmy posortowane alfabetycznie:
+authors_names = set()
+
+for _ in names:
+    authors_names.add(_.text)
+print()
+
+for author in sorted(authors_names):
+    print(author)
