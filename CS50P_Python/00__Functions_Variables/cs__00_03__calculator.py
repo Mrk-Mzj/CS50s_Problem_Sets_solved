d97db7b32cc83cd9570ec6f37e7ac310c8944e30
Mrk-Mzj <Marek.Mazij@gmail.com>
2023-04-16 01:11:17 +0200

    more precise method name given

--- CS50P_Python/00__Functions_Variables/cs__00_03__calculator.py
+++ CS50P_Python/00__Functions_Variables/cs__00_03__calculator.py
@@ -1,16 +1,16 @@
 
 def main():
 
-    # asking and assigning a float value to variables
+    # asking and assigning a float value to variables:
     x = float(input("What's X? "))
     y = float(input("What's Y? "))
 
-    adding(x,y)
+    print_adding(x,y)
 
 
-def adding(x,y):
+def print_adding(x,y):
     
-    # separating large numbers with commas and replacing them with spaces
+    # separating large numbers with commas and replacing them with spaces:
     print(f'Adding result is: { (x+y) :,}'.replace(',' , ' '))
 
 
