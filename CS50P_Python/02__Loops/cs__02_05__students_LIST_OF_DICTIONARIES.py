f415a732c7fd1b0a03797705e24c0b6570409d5b
Mrk-Mzj <Marek.Mazij@gmail.com>
2023-04-17 00:40:08 +0200

    better variables names

--- CS50P_Python/02__Loops/cs__02_05__students_LIST_OF_DICTIONARIES.py
+++ CS50P_Python/02__Loops/cs__02_05__students_LIST_OF_DICTIONARIES.py
@@ -1,24 +1,21 @@
-
 # lista [ słowników {'zbiorów':'par'} ]
 # list_of_directories = [{'name':'Hermiona', 'house':'Gryffindor', 'patronus':'Otter'}, {'name':'Harry', 'house':'Gryffindor', 'patronus':'Stag'}, itd.]
 
-list_of_directories = [
-    
-    {'name': 'Hermiona', 'house': 'Gryffindor',   'patronus': 'Otter'},
-    {'name': 'Harry',    'house': 'Gryffindor',   'patronus': 'Stag'},
-    {'name': 'Ron',      'house': 'Gryffindor',   'patronus': 'Terrier'},
-    {'name': 'Draco',    'house': 'Slytherin',    'patronus': None}
+list_of_dictionaries = [
+    {"name": "Hermiona", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None},
 ]
 
 
 # drukowanie konkretnej wartości z listy słowników
-print ('\n', list_of_directories[3]['name'],'\n')
+print("\n", list_of_dictionaries[3]["name"], "\n")
 
 
 # drukowanie kolumny z listy słowników
-for czarodziej in list_of_directories:
-    print (czarodziej['name'])
+for wizard in list_of_dictionaries:
+    print(wizard["name"])
 
 
 print()
-
