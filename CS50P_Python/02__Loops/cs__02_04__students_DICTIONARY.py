4b2e9e8708a3f2de89b758cb0ea6ffb516f14d1e
Mrk-Mzj <Marek.Mazij@gmail.com>
2023-04-17 00:35:04 +0200

    better comments

--- CS50P_Python/02__Loops/cs__02_04__students_DICTIONARY.py
+++ CS50P_Python/02__Loops/cs__02_04__students_DICTIONARY.py
@@ -1,23 +1,20 @@
-
-# słownik, czyli {'zbiór':'par'}
+# słownik, czyli zbiór par {'klucz':'wartość'}
 # dictionary = {'Hermiona':'Gryffindor', 'Harry':'Gryffindor', 'Ron':'Gryffindor', 'Draco':'Slytherin'}
 
 dictionary = {
-    
-    'Hermiona': 'Gryffindor', 
-    'Harry':    'Gryffindor', 
-    'Ron':      'Gryffindor',
-    'Draco':    'Slytherin'
+    "Hermiona": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin",
 }
 
 
 # drukowanie pojedynczej pary klucz-wartość
-print ('\n','Draco należy do', dictionary['Draco'], '\n')
+print("\n", "Draco należy do", dictionary["Draco"], "\n")
 
 
 # drukowanie wszytskich par klucz-wartość
 for address in dictionary:
-    print (address,'należy do',dictionary[address])
+    print(address, "należy do", dictionary[address])
 
 print()
-
