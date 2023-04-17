c3fb06b78589152b09d802be043d0a5796dddffc
Mrk-Mzj <Marek.Mazij@gmail.com>
2023-04-17 14:49:14 +0200

    code commented and completed

--- CS50P_Python/02__Loops/problem_set_2/pset_02_05__Nutrition_Facts.py
+++ CS50P_Python/02__Loops/problem_set_2/pset_02_05__Nutrition_Facts.py
@@ -1,3 +1,11 @@
+# implement a program that prompts consumers users to input a fruit (case-insensitively)
+# and then outputs the number of calories in one portion of that fruit.
+# Capitalization aside, assume that users will input fruits exactly as written in the poster (e.g., strawberries, not strawberry).
+# Ignore any input that isn’t a fruit.
+
+
 fruits = {"apple": 130, "banana": 110, "grape": 90, "lemon": 15, "orange": 80}
 fruit = input("Which fruit? (apple, banana, grape, lemon, orange)\n")
-print(fruits[fruit], "kcal")
+
+if fruit in fruits:
+    print(fruits[fruit], "kcal")
