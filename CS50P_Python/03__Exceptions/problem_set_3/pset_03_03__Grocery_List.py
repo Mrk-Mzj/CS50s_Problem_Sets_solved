04253fb2adc01e9170fa98c92fdcbf94ba13b617
Mrk-Mzj <Marek.Mazij@gmail.com>
2023-04-19 23:07:48 +0200

    better comments

--- CS50P_Python/03__Exceptions/problem_set_3/pset_03_03__Grocery_List.py
+++ CS50P_Python/03__Exceptions/problem_set_3/pset_03_03__Grocery_List.py
@@ -1,5 +1,7 @@
-# Odpytuj pozycje do kupna. Po wciśnięciu Ctrl+Z (znacznik końca linii) wypisz
-# wielkimi liniami, w kolejności alfabetycznej, z podaną ilością sztuk
+# Tworzymy listę zakupów.
+# Pytamy o zakup. Jeśli jest już na liście, zwiększamy liczbę sztuk o 1.
+# Po wciśnięciu Ctrl+Z (znacznik końca linii) wypisz listę zakupów
+# wielkimi liniami, w kolejności alfabetycznej, z podaną ilością sztuk.
 
 
 grocery = {}
@@ -10,24 +12,25 @@ while True:
         item = input("Add to the list: (Ctrl+Z to finish) ")
 
         try:
-            count = grocery[item.upper()] + 1  # wczytujemy ile sztuk już zapisaliśmy
+            # spróbujmy wczytać do zmiennej count, ile sztuk mamy już na liście
+            count = grocery[item.upper()]
 
-        except KeyError:  # jeśli na liście nie było takiej sztuki
+        except KeyError:  # jeśli na liście nie było takiej pozycji, wpiszmy: 1
             grocery[item.upper()] = 1
             pass
 
-        else:  # była jakaś sztuka, kontynuujemy try tutaj:
-            grocery[item.upper()] = count
+        else:  # jeśli udało się wczytać (pozycja była na liście), zwiększamy liczbę o 1:
+            grocery[item.upper()] = count + 1
 
     except EOFError:  # po wciśnięciu Ctrl+Z
         break
 
 
-# sortowanie słownika po owocach:
+# sortujemy alfabetycznie:
 grocery_sorted = {}
 for _ in sorted(grocery):
     grocery_sorted[_] = grocery[_]
 
-# drukowanie:
+# drukujemy:
 for _ in grocery_sorted:
     print(f"\n{_}: {grocery_sorted.get(_)}")
