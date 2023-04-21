0974e07b79527a876fb8afebf34e6598b25d6f8a
Mrk-Mzj <Marek.Mazij@gmail.com>
2023-04-21 21:56:09 +0200

    better somments

--- CS50P_Python/04__Libraries__parameters_and_APIs/cs__04_02__PACKAGES.py
+++ CS50P_Python/04__Libraries__parameters_and_APIs/cs__04_02__PACKAGES.py
@@ -1,14 +1,15 @@
+""" 
+instalujemy pakiet Cowsay (zewnętrzny folder z modułami) poleceniem: "pip install cowsay". Więcej ciekawych pakietów na PyPI.org
+uruchamiamy z konsoli: 
+cs__04_02__PACKAGES.py"
+"""
+
 import cowsay
 import sys
 
 print(dir(cowsay))
-# instalujemy pakiet Cowsay (zewnętrzny folder z modułami) poleceniem: "pip install cowsay". Więcej ciekawych pakietów na PyPI.org
-"""uruchamiamy z konsoli: 
-cs__04_02__PACKAGES.py"
-"""
 
 # jeśli program zostaje uruchomiony z dopisanym dokładnie 1 parametrem
 if len(sys.argv) == 2:
-
     # uruchom funkcję cow z pakietu cowsay
     cowsay.cow("Hello " + sys.argv[1])
