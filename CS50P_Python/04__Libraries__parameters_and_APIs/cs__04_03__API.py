e55508f7aa5adb8ca76fd9d4e35474a78f581b78
Mrk-Mzj <Marek.Mazij@gmail.com>
2023-04-22 21:36:28 +0200

    better comments

--- CS50P_Python/04__Libraries__parameters_and_APIs/cs__04_03__API.py
+++ CS50P_Python/04__Libraries__parameters_and_APIs/cs__04_03__API.py
@@ -1,17 +1,17 @@
-# Pomysł na użycie API. łączymy się z iTunes. Pobieramy info o piosence (entity), numer 1 (song), z zespołu Metallica (term)
+# Pomysł na użycie API. łączymy się z iTunes. 
+# Pobieramy info o piosence (entity=song), numer 1 (limit=1), z zespołu Metallica (term=metallica)
 # https://itunes.apple.com/search?entity=song&limit=1&term=metallica
 # wbicie tego w przeglądarkę zwraca surowy plik tekstowy JSON.
 
 # pip install requests - instalujemy z pip pakiet requests, który pozwala korzystać z API, jakby nasz program był przeglądarką
-# importujemy też obsługę przekazywnia parametrów (sys) podczas uruchamiania programu
+# importujemy też sys - obsługę przekazywnia parametrów podczas uruchamiania programu
 """
 python "cs__04_03__API" Metallica
 """
 import requests
 import sys
 
-# Pozwólmy użytkownikowi samemu wybrać zespół, ale zabezpieczmy się;
-# jeśli program zostanie uruchomiony bez pojedynczego argumentu, opuść go
+# Pozwólmy użytkownikowi samemu wybrać zespół. Upewnijmy się, że user podał dokładnie 1 argument:
 if len(sys.argv) != 2:
     sys.exit()
 
@@ -20,8 +20,8 @@ server_JSON = requests.get(
     "https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1]
 )
 
-# tego pliku nie da się wprost wydrukować; otrzymalibyśmy cyfrę 200 (komunikat, że serwer jest ok)
-# konwertujemy ten plik do obiektu - słownika Pythona:
+# tego PLIKU nie da się wprost wydrukować; otrzymalibyśmy cyfrę 200 (komunikat, że serwer jest ok)
+# konwertujemy ten plik do OBIEKTU, słownika Pythona:
 python_JSON_dict = server_JSON.json()
 print("\n", python_JSON_dict)
 
