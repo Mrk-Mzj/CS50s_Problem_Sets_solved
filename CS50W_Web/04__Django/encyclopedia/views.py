7f2a9a783f7bbd4094662e2b15f4d5eb1b565cd1
Mrk-Mzj <Marek.Mazij@gmail.com>
2024-02-01 22:20:03 +0100

    feat: search function

--- CS50W_Web/04__Django/encyclopedia/views.py
+++ CS50W_Web/04__Django/encyclopedia/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from . import util
@@ -25,3 +26,29 @@ def entry(request, title):
             "encyclopedia/entry.html",
             {"title": "404 - no entry", "content": "404 - no entry found"},
         )
+
+
+def search(request):
+    # show entry page searched by user
+    # TODO: consider joining entry (GET) and search (POST)
+    searched = request.POST["q"].strip()
+
+    # show entry if searched matches
+    if util.get_entry(searched):
+        return render(
+            request,
+            "encyclopedia/entry.html",
+            {
+                "title": searched,
+                "content": markdown2.markdown(util.get_entry(searched)),
+            },
+        )
+    # look for similar entries
+    else:
+        results = []
+        for _ in util.list_entries():
+            if searched.lower() in _.lower():
+                results.append(_)
+
+        # results = util.list_entries().filter(searched)
+        return render(request, "encyclopedia/search.html", {"results": results})
